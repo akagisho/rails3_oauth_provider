6e55f84ee6a60dbdc4f1ad5e07ed87199c5aaab0
akagisho <akagisho@gmail.com>
2013-11-02 12:25:49 +0900

    Update a lot

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  alias :login_required :authenticate_user!
+  def current_user= user
+    current_user = user
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  has_many :client_applications
+  has_many :tokens, class_name: "Oauth2Token", order: "authorized_at desc", include: [:client_application]
 end
